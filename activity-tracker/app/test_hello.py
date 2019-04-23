2b4bdb2046cf3a2ab55ee4c5428d54f8fc57860e
miafrank <mia.frank@daugherty.com>
2019-04-23 11:38:00 -0500

    added hello world and hello world unit test

---
+++ activity-tracker/app/test_hello.py
@@ -0,0 +1,5 @@
+import hello
+
+def test_hello():
+    hi = hello.hi()
+    assert hi == "Hello World"
--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from flask import Flask
+#!/usr/bin/env python
 
-app = Flask(__name__)
+from distutils.core import setup
+from glob import glob
+from setuptools import find_packages
 
-@app.route("/")
-def hello(): 
-    return "Hello World"
-
-if __name__ == '__main__':
-     app.run(port='8080')
+setup(name='activity-rest-api',
+      version='1.0',
+      description='Python REST Api',
+      author='Mia Frank',
+      packages=find_packages('activity-tracker'),
+      package_dir={'': 'activity-tracker'},
+      py_modules=[splitext(basename(path))[0] for path in glob('activity-tracker/*.py')],
+     )
