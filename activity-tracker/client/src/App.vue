07c1b3c67432621b326bb7a085f3aa80870840da
miafrank <mia.frank@daugherty.com>
2019-05-13 10:56:59 -0500

    removed boilerplate vue code

--- activity-tracker/client/src/App.vue
+++ activity-tracker/client/src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/activities">Activities</router-link>
     </div>
     <router-view/>
   </div>
