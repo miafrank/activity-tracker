3dcf45c227ef91e4c2e2fd5e40893235c75f90b4
miafrank <mia.frank@daugherty.com>
2019-05-13 11:44:03 -0500

    got data back from api

--- activity-tracker/client/src/views/Activities.vue
+++ activity-tracker/client/src/views/Activities.vue
@@ -1,3 +1,32 @@
 <template>
-<p>Testing testing</p>
+  <div>
+    <ul id="activities">
+      <li v-for="activity in activities" v-bind:key="activity.activity_id">
+          {{ activity.activity_id }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      activities: [],
+    };
+  },
+  methods: {
+    getActivities() {
+      const path = 'http://localhost:5000/activities';
+      axios.get(path)
+        .then((res) => {
+          this.activities = res.data.activities;
+        });
+    },
+  },
+  created() {
+    this.getActivities();
+  },
+};
+</script>
