f7f423753f83791539845208bf7b72e2564d4fdb
miafrank <mia.frank@daugherty.com>
2019-05-17 11:32:23 -0500

    added unit test to make sure component can be mounted correctly

--- activity-tracker/client/tests/unit/example.spec.js
+++ activity-tracker/client/tests/unit/example.spec.js
@@ -1,7 +1,12 @@
-import { expect } from 'chai';
+import Activities from '@/views/Activities.vue';
+import { mount } from '@vue/test-utils';
+// mount creates wrapper that contains the mounted (mocked) component
+// var assert = require('assert');
+var expect = require('chai').expect;
 
-describe('first test', () => {
-  it('is true', () => {
-    expect(true).to.equal(true);
+describe('Activities.vue', () => {
+  it('mounts component', () => {
+    const wrapper = mount(Activities);
+    expect(wrapper.isVueInstance()).to.be.true;
   });
 });
