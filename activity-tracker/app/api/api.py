b518a36ab8acbc226cfc68b29eca12cfa0f37034
mia frank <miajfrank12@gmail.com>
2019-11-10 11:13:47 -0600

    renamed flask app, moved config file

--- activity-tracker/app/api/api.py
+++ activity-tracker/app/api/api.py
@@ -4,24 +4,24 @@ from app.dynamodb import dynamodb_resource
 from flask_cors import CORS
 
 
-application = Flask(__name__)
+app = Flask(__name__)
 activity_table_name = 'activity'
-enable_cors_for_all_routes = CORS(application)
+enable_cors_for_all_routes = CORS(app)
 
 
-@application.route("/activities", methods=['GET'])
+@app.route("/activities", methods=['GET'])
 def get_all_items():
     return jsonify(activities=aws_dynamo_controller.get_all_items(activity_table_name))
 
 
-@application.route('/activities', methods=['POST'])
+@app.route('/activities', methods=['POST'])
 def create_activity():
     request_data = request.get_json()
     activity_id = aws_dynamo_controller.create_new_item(request_data, dynamodb_resource.set_up_resource())
     return jsonify(id=activity_id)
 
 
-@application.route('/activities/<activity_id>', methods=['GET'])
+@app.route('/activities/<activity_id>', methods=['GET'])
 def get_activity_by_id(activity_id):
     item = \
         aws_dynamo_controller.get_item_by_id(
@@ -31,7 +31,7 @@ def get_activity_by_id(activity_id):
     return jsonify(item=item)
 
 
-@application.route('/activities/<activity_id>', methods=['PUT'])
+@app.route('/activities/<activity_id>', methods=['PUT'])
 def update_activity_by_id(activity_id):
     request_data = request.get_json()
     update_item = \
@@ -43,7 +43,7 @@ def update_activity_by_id(activity_id):
     return jsonify(update_item)
 
 
-@application.route('/activities/<activity_id>', methods=['DELETE', 'POST'])
+@app.route('/activities/<activity_id>', methods=['DELETE', 'POST'])
 def delete_activity_by_id(activity_id):
     deleted_item = \
         aws_dynamo_controller.delete_item_by_id(
@@ -54,4 +54,4 @@ def delete_activity_by_id(activity_id):
 
 
 if __name__ == '__main__':
-    application.run(port='5000')
+    app.run(port='5000')
