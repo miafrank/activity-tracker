118a30052853819a2dadfed1952ab26519f3580a
mia frank <miajfrank12@gmail.com>
2019-11-12 06:37:30 -0600

    added util method to parse response

--- activity-tracker/app/utils.py
+++ activity-tracker/app/utils.py
@@ -1,12 +1,21 @@
 import uuid
 
 
-def get_rows(item):
+def get_row_by_column_name(item):
     return {
         'activity_id': item['id'].get('S'),
         'activity_date': item['activity_date'].get('S'),
         'activity_name': item['activity_name'].get('S'),
         'activity_duration': item['activity_duration'].get('S')
+    }
+
+
+def get_rows(item):
+    return {
+        'activity_id': item['id'],
+        'activity_date': item['activity_date'],
+        'activity_name': item['activity_name'],
+        'activity_duration': item['activity_duration']
     }
 
 
