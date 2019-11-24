3af556c05f59145837946bef7b3267f271e56be9
mia frank <miajfrank12@gmail.com>
2019-11-24 10:35:50 -0600

    updated logic with dynamo client and resource from api layer

--- activity-tracker/app/dynamodb/dynamodb_utils.py
+++ activity-tracker/app/dynamodb/dynamodb_utils.py
@@ -2,43 +2,39 @@ from flask_api import status
 
 import logging
 
-from . import dynamodb_resource_service
 from app import utils
 from app.config import ITEM_NOT_FOUND, ITEM_DELETED_SUCCESSFULLY
 
-dynamodb_client = dynamodb_resource_service.dynamodb_client()
-dynamodb_resource = dynamodb_resource_service.dynamodb_resource()
 
-
-def get_all_items():
-    items = [utils.get_rows(item) for item in dynamodb_client['Items']]
+def get_all_items(client):
+    items = [utils.get_rows(item) for item in client['Items']]
     return items
 
 
-def create_new_item(json):
+def create_new_item(json, resource):
     json['id'] = utils.generate_uuid()
-    dynamodb_resource.put_item(Item=json)
+    resource.put_item(Item=json)
     return json
 
 
-def get_item_by_id(activity_id):
-    response = dynamodb_resource.get_item(Key={'id': str(activity_id)})
+def get_item_by_id(activity_id, resource):
+    response = resource.get_item(Key={'id': str(activity_id)})
     return response
 
 
-def get_activity_by_id(activity_id):
-    response = get_item_by_id(activity_id)
+def get_activity_by_id(activity_id, resource):
+    response = get_item_by_id(activity_id, resource)
     return utils.get_rows(response['Item']) if 'Item' in response.keys() else ITEM_NOT_FOUND
 
 
-def update_item_by_id(activity_id, payload):
+def update_item_by_id(activity_id, payload, resource):
     # TODO - this is gross but if all values are not passed update expression, exception is raised.
     # it is kind of a hassle to update one by one, so for now i am parsing the json so that all values
     # in expression update are passed to satisfy boto3
     activity_date, activity_duration, activity_name = (payload['activity_date'],
                                                        payload['activity_name'],
                                                        payload['activity_duration'])
-    return dynamodb_resource.update_item(
+    return resource.update_item(
         Key={'id': str(activity_id)},
         UpdateExpression="set activity_date = :date, activity_name= :name, activity_duration = :duration",
         ExpressionAttributeValues={
@@ -54,7 +50,7 @@ def item_deleted_successfully(response):
     return ITEM_DELETED_SUCCESSFULLY if http_response == status.HTTP_200_OK else logging.info("Something went wrong")
 
 
-def delete_item_by_id(activity_id):
-    if get_activity_by_id(activity_id):
-        deleted_item = dynamodb_resource.delete_item(Key={'id': str(activity_id)})
+def delete_item_by_id(activity_id, resource):
+    if get_activity_by_id(activity_id, resource):
+        deleted_item = resource.delete_item(Key={'id': str(activity_id)})
         return item_deleted_successfully(deleted_item)
