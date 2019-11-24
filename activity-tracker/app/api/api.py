f5b311be32551378184beb5ffce426baab5f48b6
mia frank <miajfrank12@gmail.com>
2019-11-24 10:35:10 -0600

    updated api with dynamo client

--- activity-tracker/app/api/api.py
+++ activity-tracker/app/api/api.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from app.dynamodb import dynamodb_utils
+from app.dynamodb.dynamodb_resource_service import dynamodb_client
+from app.dynamodb.dynamodb_resource_service import dynamodb_resource
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,28 +10,28 @@ enable_cors_for_all_routes = CORS(app)
 
 @app.route("/activities", methods=['GET'])
 def get_all_activities():
-    return jsonify(dynamodb_utils.get_all_items())
+    return jsonify(dynamodb_utils.get_all_items(dynamodb_client()))
 
 
 @app.route('/activities', methods=['POST'])
 def create_new_activity():
-    return jsonify(id=dynamodb_utils.create_new_item(request.get_json())['id'])
+    return jsonify(id=dynamodb_utils.create_new_item(request.get_json(), dynamodb_resource())['id'])
 
 
 @app.route('/activities/<activity_id>', methods=['GET'])
 def get_activity_by_id(activity_id):
-    return jsonify(item=dynamodb_utils.get_activity_by_id(activity_id))
+    return jsonify(item=dynamodb_utils.get_activity_by_id(activity_id, dynamodb_resource()))
 
 
 @app.route('/activities/<activity_id>', methods=['PUT'])
 def update_activity_by_id(activity_id):
     request_data = request.get_json()
-    return jsonify(dynamodb_utils.update_item_by_id(activity_id, request_data))
+    return jsonify(dynamodb_utils.update_item_by_id(activity_id, request_data, dynamodb_resource()))
 
 
 @app.route('/activities/<activity_id>', methods=['DELETE', 'POST'])
 def delete_activity_by_id(activity_id):
-    return jsonify(item=dynamodb_utils.delete_item_by_id(activity_id))
+    return jsonify(item=dynamodb_utils.delete_item_by_id(activity_id, dynamodb_resource()))
 
 
 if __name__ == '__main__':
